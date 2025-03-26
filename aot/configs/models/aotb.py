8b5b50cea10bdc97e0cf90109d09d3b93cd260f1
Zhao Weikun <82690855+zhao-wk@users.noreply.github.com>
2025-03-26 19:59:35 +0800

    Delete aot directory

--- aot/configs/models/aotb.py
+++
@@ -1,9 +0,0 @@
-import os
-from .default import DefaultModelConfig
-
-class ModelConfig(DefaultModelConfig):
-    def __init__(self):
-        super().__init__()
-        self.MODEL_NAME = 'AOTB'
-
-        self.MODEL_LSTT_NUM = 3
--- aot/networks/decoders/__init__.py
+++
@@ -1,9 +0,0 @@
-from networks.decoders.fpn import FPNSegmentationHead
-
-
-def build_decoder(name, **kwargs):
-
-    if name == 'fpn':
-        return FPNSegmentationHead(**kwargs)
-    else:
-        raise NotImplementedError
